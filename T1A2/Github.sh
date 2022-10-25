d7fa8f25e16e78866fd93f5ccb19be5d2906f660
David Perez <dperezc01@gmail.com>
2022-10-25 16:21:46 +0200

    initial commit

--- T1A2/Github.sh
+++ T1A2/Github.sh
@@ -1,8 +1,8 @@
 if [ $# -ge 1 ]; then
     git add ..
-    git commit -m "Automatic update"
+    git commit -m "initial commit"    
     git show-ref
-    git push origin master --force
+    git push origin master
 else
     echo "Introduce El nombre de usuario como parámetro"
 fi
