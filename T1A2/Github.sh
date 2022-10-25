4c0795cc4b57aa6613a221a4cf5f068db2f2aec0
David Perez <dperezc01@gmail.com>
2022-10-25 16:19:33 +0200

    Automatic update

--- T1A2/Github.sh
+++ T1A2/Github.sh
@@ -1,6 +1,7 @@
 if [ $# -ge 1 ]; then
     git add ..
     git commit -m "Automatic update"
+    git show-ref
     git push origin master 
 else
     echo "Introduce El nombre de usuario como parámetro"
