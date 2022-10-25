03686c0d2300a0ac95952940f07693b681110333
David Perez <dperezc01@gmail.com>
2022-10-25 16:08:48 +0200

    First commit

--- T1A2/Github.sh
+++ T1A2/Github.sh
@@ -1,8 +1,10 @@
 if [ $# -ge 1 ]; then
+    git config --global pull.rebase true
+    git config --global rebase.autoStash true  
     git init .. -b main
     git add .
     git commit -m "First commit"
-    git remote add origin "https://github.com/Unownisgod/PSP-scripts"
+    git remote add origin "https://github.com/Unownisgod/PPS-scripts"
     git remote -v
     git push origin main
 else
