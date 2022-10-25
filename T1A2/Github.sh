c715b86051498b98e2df3e03dcbdb9ff2c5406ca
David Perez <dperezc01@gmail.com>
2022-10-25 16:28:00 +0200

    initial commit

--- T1A2/Github.sh
+++ T1A2/Github.sh
@@ -3,6 +3,7 @@ if [ $# -ge 1 ]; then
     git add ..
     git commit -m "initial commit"    
     git show-ref
+    git pull origin master
     git push origin master
 else
     echo "Introduce El nombre de usuario como parámetro"
