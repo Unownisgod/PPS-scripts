a3df7afeb4b39af5ff7d949e98184af70b92955b
David Perez <dperezc01@gmail.com>
2022-10-25 16:14:20 +0200

    First commit

--- T1A2/Github.sh
+++ T1A2/Github.sh
@@ -2,11 +2,12 @@ if [ $# -ge 1 ]; then
     git config --global pull.rebase true
     git config --global rebase.autoStash true  
     git init .. -b main
+    git commit -m "First commit"
     git remote add origin "https://github.com/Unownisgod/PPS-scripts"
     git remote -v
     git add .
-    git commit -m "First commit"
-
+    git pull -u origin main
+    git push "https://github.com/Unownisgod/PPS-scripts"
 else
     echo "Introduce El nombre de usuario como parámetro"
 fi
