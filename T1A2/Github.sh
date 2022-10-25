120ffad647e0a848c8bd207813349ffa5697cc8d
Unowni <Unownisgod>
2022-10-25 16:40:53 +0200

    Automatic update

--- T1A2/Github.sh
+++ T1A2/Github.sh
@@ -1,5 +1,5 @@
-if [ $# -ge 1 ]; then
-    git config --global user.email "$1"
+if [ $# -ge 1 ]; then #Comprueba si se le ha pasado algún parámetro
+    git config --global user.email "$1" #introduce el nombre de usuario de git
     git init ..
     git add ..
     git commit -m "Automatic update"    
